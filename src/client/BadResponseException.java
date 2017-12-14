7db33c79f0fb56a14e488c5deb7518ac9a3ba7ea
Darkluke1111 <lukas.gromer@freenet.de>
2017-12-14 10:25:30 +0100

    Cleaned up Code

--- src/client/BadResponseException.java
+++ src/client/BadResponseException.java
@@ -1,4 +1,12 @@
 package client;
 
 public class BadResponseException extends Exception {
+
+  public BadResponseException(String msg) {
+    super(msg);
+  }
+
+  public BadResponseException() {
+
+  }
 }
--- src/client/WebClient.java
+++ src/client/WebClient.java
@@ -8,55 +8,56 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 
-public class Client {
+public class WebClient {
+
+  InetAddress address;
+  int port;
+
+
+  public WebClient(InetAddress address, int port) {
+    this.address = address;
+    this.port = port;
+  }
 
   public static void main(String[] args) throws IOException, BadResponseException {
-    Client c = new Client();
+    WebClient c = new WebClient(InetAddress.getByName("134.60.77.151"), 7777);
 
     File f = new File("test.jpg");
+    File f2 = new File("converted.jpg");
     System.out.println(f.getCanonicalPath());
-    c.sendRequest(InetAddress.getByName("134.60.77.151"),7777,(byte) 0,f);
+    c.sendRequest((byte) 1, f, f2);
   }
 
 
-  public void sendRequest(InetAddress address, int port, byte filter, File imageFile) throws BadResponseException {
+  public void sendRequest(byte filter, File imageFile, File targetFile) throws BadResponseException {
     try (
         Socket socket = new Socket(address, port);
         OutputStream out = socket.getOutputStream();
         InputStream in = socket.getInputStream();
-        FileInputStream fi = new FileInputStream(imageFile)
+        FileInputStream fi = new FileInputStream(imageFile);
+        FileOutputStream fo = new FileOutputStream(targetFile)
     ) {
 
+      //Send Request /////////////////////
       //send the filter
       out.write(filter);
 
       //send the file length
       System.out.println(imageFile.length());
-      out.write(ByteBuffer.allocate(4).putInt((int)imageFile.length()).array());
+      out.write(ByteBuffer.allocate(4).putInt((int) imageFile.length()).array());
 
       //send the file
-      {
-        int total = 0;
-        int count;
-        byte[] buffer = new byte[1024];
-        while ((count = fi.read(buffer)) > 0) {
-          total += count;
-          System.out.println(total);
-          out.write(buffer, 0, count);
-        }
-      }
+      connectStreams(fi,out);
 
-      out.flush();
-
+      // Read Response ///////////////////
       //read response status
       int status = in.read();
 
       //throw Exception if there was an error
       if (status != 0) {
-        throw new BadResponseException();
+        throw new BadResponseException("An Error occured! Error Code: " + status);
       }
 
       //read response file length
@@ -65,21 +66,21 @@ public class Client {
       ByteBuffer bb = ByteBuffer.wrap(b);
       int responseLength = bb.getInt();
 
-      //read file
-      File responseFile = new File("converted.jpg");
-      responseFile.createNewFile();
-      try(FileOutputStream fo = new FileOutputStream(responseFile)) {
-        int count;
-        byte[] buffer = new byte[1024];
-        while ((count = in.read(buffer)) > 0) {
-          fo.write(buffer, 0, count);
-        }
-        fo.flush();
-      }
+      //save received file file
+      connectStreams(in,fo);
 
 
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  private void connectStreams(InputStream in, OutputStream out) throws IOException {
+    int count;
+    byte[] buffer = new byte[1024];
+    while ((count = in.read(buffer)) > 0) {
+      out.write(buffer, 0, count);
+    }
+    out.flush();
   }
 }
