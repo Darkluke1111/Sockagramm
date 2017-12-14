375ec7ae50d9b63116b1f660ef7bafe74f15d25d
Darkluke1111 <lukas.gromer@freenet.de>
2017-12-14 10:50:17 +0100

    Cleaned up Code

--- src/client/View.java
+++ src/client/View.java
@@ -1,10 +1,12 @@
 package client;
 
+import java.io.File;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -14,6 +16,7 @@ public class View {
   BorderPane root;
   Stage stage;
   Scene scene;
+  File defaultImage = new File("test.jpg");
 
   public View(Stage stage) {
 
@@ -24,14 +27,14 @@ public class View {
     Button selectFilter = new Button("Select Filter");
     Button selectInput = new Button("Select Image");
 
-    gp.add(selectFilter,1,1);
-    gp.add(selectInput,2,1);
+    gp.add(selectFilter, 1, 1);
+    gp.add(selectInput, 2, 1);
 
-    ImageView inputImage = new ImageView();
+    ImageView inputImage = new ImageView(new Image(defaultImage.toURI().toString()));
     ImageView outputImage = new ImageView();
 
-    gp.add(inputImage,2,1);
-    gp.add(outputImage,2,2);
+    gp.add(inputImage, 1, 2);
+    gp.add(outputImage, 2, 2);
 
     root.setCenter(gp);
 
@@ -44,4 +47,5 @@ public class View {
     scene = new Scene(root);
     stage.setScene(scene);
   }
+
 }
